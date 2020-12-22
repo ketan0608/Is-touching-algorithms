85453d691434628d358bc832763bd01d400f42ab
ketan0608 <76109939+ketan0608@users.noreply.github.com>
2020-12-22 19:50:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+var fixedrect,movingrect;
+
+function setup() {
+  createCanvas(800,400);
+ movingrect = createSprite(400, 200, 80, 30);
+ movingrect.shapeColor = "yellow";
+ fixedrect = createSprite(200,200,50,50);
+ fixedrect.shapeColor = "yellow";
+}
+
+function draw() {
+  background(255,255,255);  
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+
+  if(movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2&&
+    fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2&&
+    movingrect.y-fixedrect.y<fixedrect.height/2+movingrect.height/2&&
+    fixedrect.y-movingrect.y<fixedrect.height/2+movingrect.height/2 ){
+  movingrect.shapeColor="red";
+  fixedrect.shapeColor="red";
+  }
+  else{
+    movingrect.shapeColor="yellow";
+  fixedrect.shapeColor="yellow";
+  }
+  
+  drawSprites();
+}
